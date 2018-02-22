4fbb4dc8ec02c35735db9b4d72a59aa3cc762863
Matt Hutchison <matt.hutchison@gmail.com>
2018-02-22 18:32:47 -0500

    Add FilePatternMonitor class.
    FilePatternMonitor is an async directory monitor that will be used to
    poll local and network-mounted filesystems for changes.
    
    Currently is intended to be compatible with:
    - NFS
    - SMB/CIFS
    - Local
    
    Others may work, but testing is required.

---
+++ monitor.py
@@ -0,0 +1,54 @@
+import asyncio
+import glob
+import time
+
+class FilePatternMonitor():
+    """Async infinite generator for listing new files matching a pattern
+    
+    Strictly monitors only the *addition* of files. Anything removed from the
+        directory is not tracked. Anything removed and then re-added will be
+        reported again.
+
+    Keyword arguments:
+    recursive -- as the "recursive" argument from glob.glob
+    walltime -- time in seconds that the directory may remain unchanged before the
+        monitor raises StopAsyncIteration and ends.
+    
+    Usage example:
+    
+    loop = asyncio.get_event_loop()
+    test = FilePatternMonitor('./*', walltime=25)
+    async def tmp():
+        while True:
+            print(await test)
+            await asyncio.sleep(5)
+    loop.run_until_complete(tmp())
+    
+    """
+    
+    def __init__(self, pattern, recursive=False, walltime=43200):
+        self.pattern = pattern
+        self.recursive = recursive
+        self.walltime = walltime
+        self.old = set()
+        self.base_time = time.time()
+
+    def __await__(self):
+        return self._get_new_files().__await__()
+
+    async def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        if self.base_time + self.walltime < time.time():
+            raise StopAsyncIteration
+        else:
+            return await self._get_new_files()
+
+    async def _get_new_files(self):
+        new = set(glob.glob(self.pattern, recursive=self.recursive))
+        temp = new.difference(self.old)
+        self.old = new
+        if temp:
+            self.base_time = time.time()
+        return sorted(temp)
