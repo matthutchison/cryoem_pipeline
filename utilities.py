2d0c205b2bbbcdfdbceee09d0746cefa853f7ac9
Matt Hutchison <matt.hutchison@gmail.com>
2018-02-26 16:39:29 -0500

    Updates utilities to handle Path objects
    Shutil.copy2 and the subprocess functions need path strings

--- utilities.py
+++ utilities.py
@@ -8,7 +8,7 @@ def safe_copy_file(src, dest):
         pathlib.Path(dest).mkdir(parents=True, exist_ok=True)
     except FileExistsError as e:
         pass
-    shutil.copy2(src, dest)
+    shutil.copy2(str(src), str(dest))
 
 
 async def compress_file(path):
@@ -16,7 +16,7 @@ async def compress_file(path):
 
     Defaults are set for the ATC linux box to not saturate. Adjust if necessary
     '''
-    cmd = ['lbzip2', '-k', '-n 8', '-z', path.expanduser().resolve()]
+    cmd = ['lbzip2', '-k', '-n 8', '-z', str(path)]
     process = await asyncio.create_subprocess_exec(*cmd)
     return await process.wait()
 
@@ -24,6 +24,6 @@ async def compress_file(path):
 async def stack_files(in_paths, out_path):
     '''Stack the files using imod. Return only after stacking complete.
     '''
-    cmd = ['newstack', '-bytes 0', *in_paths, out_path]
+    cmd = ['newstack', '-bytes 0', *[str(p) for p in in_paths], str(out_path)]
     process = await asyncio.create_subprocess_exec(*cmd)
     return await process.wait()
