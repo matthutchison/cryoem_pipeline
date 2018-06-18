7777944d4fb43ce1bb3c77dd6a38f8feea0bccf4
Matt Hutchison <matt.hutchison@gmail.com>
2018-06-18 16:04:49 -0400

    Adds tests of safe_copy_file and updates others
    - Adds tests for safe_copy_file
      - confirms that FileExistsError is raised appropriately
      - confirms that safe_copy_file copies to dest
    - Cleanups on the existing tests

--- test/test_workflow.py
+++ test/test_workflow.py
@@ -1,7 +1,10 @@
 import workflow.utilities as util
 import asyncio
+import pathlib
 import unittest
 import tempfile
+
+named_temp = tempfile.NamedTemporaryFile
 
 
 class UtilitiesTests(unittest.TestCase):
@@ -27,35 +30,49 @@ class UtilitiesTests(unittest.TestCase):
         self.loop.run_until_complete(task)
 
     def test_hash_succeeds_on_real_path(self):
-        with tempfile.NamedTemporaryFile() as f:
+        with named_temp() as f:
             func = util.file_hash(f.name)
             task = self.loop.create_task(func)
             task.add_done_callback(self.future_has_no_stderr_content)
             self.loop.run_until_complete(task)
 
     def test_compare_hash_succeeds_on_identical_content(self):
-        with tempfile.NamedTemporaryFile() as f1:
-            with tempfile.NamedTemporaryFile() as f2:
-                func = util.compare_hashes(f1.name, f2.name)
-                task = self.loop.create_task(func)
-                task.add_done_callback(
-                    lambda fut: self.assertTrue(fut.result()))
-                self.loop.run_until_complete(task)
+        with named_temp() as f1, named_temp() as f2:
+            func = util.compare_hashes(f1.name, f2.name)
+            task = self.loop.create_task(func)
+            task.add_done_callback(
+                lambda fut: self.assertTrue(fut.result()))
+            self.loop.run_until_complete(task)
 
     def test_compare_hash_fails_on_different_content(self):
-        with tempfile.NamedTemporaryFile() as f1:
-            with tempfile.NamedTemporaryFile() as f2:
-                f1.write(b'content1')
-                f1.flush()
-                func = util.compare_hashes(f1.name, f2.name)
-                task = self.loop.create_task(func)
-                task.add_done_callback(
-                    lambda fut: self.assertFalse(fut.result()))
-                self.loop.run_until_complete(task)
+        with named_temp() as f1, named_temp() as f2:
+            f1.write(b'content1')
+            f1.flush()
+            func = util.compare_hashes(f1.name, f2.name)
+            task = self.loop.create_task(func)
+            task.add_done_callback(
+                lambda fut: self.assertFalse(fut.result()))
+            self.loop.run_until_complete(task)
 
     def test_compare_hash_raises_error_on_fake_path(self):
-        with tempfile.NamedTemporaryFile() as f1:
+        with named_temp() as f1:
             func = util.compare_hashes(f1.name, self.fake_path)
             task = self.loop.create_task(func)
             with self.assertRaises(FileNotFoundError):
                 self.loop.run_until_complete(task)
+
+    def test_safe_copy_fails_when_dest_exists(self):
+        with named_temp() as f1, named_temp() as f2:
+            with self.assertRaises(FileExistsError):
+                self.loop.run_until_complete(
+                    self.loop.create_task(
+                        util.safe_copy_file(f1.name, f2.name)))
+
+    def test_safe_copy_succeeds_when_dest_does_not_exists(self):
+        with named_temp() as f1:
+            newpath = f1.name+'testloc'
+            self.loop.run_until_complete(
+                self.loop.create_task(
+                    util.safe_copy_file(f1.name, newpath))
+            )
+            self.assertTrue(pathlib.Path(newpath).exists())
